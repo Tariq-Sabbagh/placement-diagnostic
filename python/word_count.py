2c6c9380938793ed0fd77bb0d89345b568f76fdd
TariqSp <teto75928@gmail.com>
2026-06-27 17:28:21 +0300

    complete the first task python (Word Frequencies)

--- python/word_count.py
+++ python/word_count.py
@@ -8,7 +8,29 @@
 def main():
     # TODO: read python/word_count_input.txt, tally each lowercased word
     # (strip surrounding punctuation), and print the top 3.
-    ...
+    word = open("word_count_input.txt")
+    string = word.read().lower()
+    string = string.replace(',' ,'')
+    string = string.replace('.','')
+    words = string.split()
+    dic = {key: 0 for key in words}
+    for w in words:
+        dic[w]+=1
+        # print(dic[w])
+    
+    diclist = list(dic.items())
+    n = len(diclist)
+    # print(diclist)
+    for i in range(n-1):
+      for j in range(n-i-1):
+        if diclist[j][1] < diclist[j+1][1]:
+          diclist[j], diclist[j+1] = diclist[j+1], diclist[j]
+        elif diclist[j][1] == diclist[j+1][1]:
+           if diclist[j][0][0] > diclist[j+1][0][0]:
+              diclist[j], diclist[j+1] = diclist[j+1], diclist[j]
+    
+    for i in range(3):
+     print(diclist[i][0])
 
 
 if __name__ == "__main__":
